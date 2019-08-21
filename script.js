941282fc6f74c7a9708134b21105a19f0f2a478c
Airgear <airgear1989@gmail.com>
2019-08-21 15:57:43 +0300

    two commit

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-function f1(){
-    var obj1,obj2;
-        obj1 = document.getElementById('inp1').value;
-        obj2 = document.getElementById('inp2').value;
-    if (obj1 == 0 && obj2 == 0){ 
-        alert('Заполните поле')
-    } 
-    else if(obj1 === obj2){
-        alert(true);
+function f1() {
+    var obj1 = document.getElementById('inp1');
+    var obj2 = document.getElementById('inp2');
+    if (obj1.value === '' && obj2.value === '') {
+        alert('Заполните поле');
+    } else if (obj1.value === obj2.value){
+        alert('Значения совпадают');
+    } else {
+        alert('Значения не совпадают');
     }
-    else if(Object.keys(obj1).length !== Object.keys(obj2).length){ 
-            alert(false);
-         }
-    for(var propName in obj1){
-        if (! obj2.hasOwnProperty(propName)) { 
-                alert(false);
-        } 
-        else if(obj1[propName].valueOf() !== obj2[propName].valueOf()){ 
-            if(! f1(obj1[propName], obj2[propName]) ){ 
-                    alert(false);
-                }
-    
+}  
+function f2(arr) {
+    var arr = document.getElementById('inp3').value;
+    let count = 0;
+    // var arr = JSON.parse(a)
+    const result = arr => arr.reduce((flatArray) => {
+        return flatArray.concat(Array.isArray(item) ? result(item): item);
+    },[]);
+    const flatArray = flatten (arr);
+    flatArray.forEach((value) =>{
+        if (value === item){
+            count +=1;
         }
-    }
-} 
-function f2(){
+    })
+    
 
+    alert(result); 
+    return count;
 }
+   
+
